10722924a14733a0195043ccc8fa83e4048a4e49
Emmanuel Ogbizi <iamogbz+github@gmail.com>
2019-04-17 14:17:04 -0400

    Revert "Handle paging of GitHub requests (#1)"
    This reverts commit 2af740a0f32a331b7ccbafa3f1e7b730fcd7e48b.

--- src/lib/GitHub.js
+++ src/lib/GitHub.js
@@ -1,23 +1,12 @@
-import url from 'url'
 import request from 'superagent'
-import parseLinkHeader from 'parse-link-header'
 
 const GITHUB_API_BASE = 'https://api.github.com'
-
-const getNextPathFromLinkHeader = resp => {
-    if (!resp.headers.link) return null
-    const link = parseLinkHeader(resp.headers.link)
-    if (!link.next) return null
-
-    const nextURL = new url.URL(link.next.url)
-    return nextURL.pathname + nextURL.search
-}
 
 export default class GitHub {
     constructor({ githubAuthToken }) {
         this.githubAuthToken = githubAuthToken
     }
-    getResponse(path) {
+    getPath(path = '/') {
         const githubAuthToken = this.githubAuthToken
         return new Promise((resolve, reject) => {
             const newRequest = request.get(`${GITHUB_API_BASE}${path}`)
@@ -30,28 +19,8 @@ export default class GitHub {
 
                 if (!res.body) reject(new Error(`Invalid GitHub response.`))
 
-                resolve(res)
+                resolve(res.body)
             })
-        })
-    }
-    getPath(path = '/') {
-        return new Promise((resolve, reject) => {
-            this.getResponse(path)
-                .then(thisPageResp => {
-                    const thisPageResults = thisPageResp.body
-                    const nextPath = getNextPathFromLinkHeader(thisPageResp)
-                    if (!nextPath) {
-                        resolve(thisPageResults)
-                        return
-                    }
-
-                    this.getPath(nextPath)
-                        .then(nextPageResults => {
-                            resolve(thisPageResults.concat(nextPageResults))
-                        })
-                        .catch(err => reject(err))
-                })
-                .catch(err => reject(err))
         })
     }
 }
